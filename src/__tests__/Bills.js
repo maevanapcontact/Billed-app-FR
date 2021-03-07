81df0414f0e14f9f4c2b9d5d79042085d6236b41
maevanapcontact <maevanap.contact@gmail.com>
2021-03-07 14:04:26 +0100

    statement coverage Bill handleClickIconEye

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,21 +1,100 @@
-import { screen } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
+import { screen, fireEvent } from "@testing-library/dom";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
+import Router from "../app/Router";
+import Bills from "../containers/Bills.js";
+import userEvent from "@testing-library/user-event";
+
+const data = [];
+const loading = false;
+const error = null;
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
-    })
+      // const onNavigate = (pathname) => {
+      //   document.body.innerHTML = ROUTES({ pathname });
+      // };
+      // Object.defineProperty(window, "localStorage", {
+      //   value: localStorageMock,
+      // });
+      // window.localStorage.setItem(
+      //   "user",
+      //   JSON.stringify({
+      //     type: "Employee",
+      //   })
+      // );
+      // const allBills = new Bills({
+      //   document,
+      //   onNavigate,
+      //   firestore: null,
+      //   localStorage: window.localStorage,
+      // });
+      // const pathname = ROUTES_PATH["Bills"];
+      // const html = ROUTES({
+      //   pathname,
+      //   data: allBills.getBills(),
+      //   loading,
+      //   error,
+      // });
+      // console.log(html);
+      // document.body.innerHTML = html;
+      // expect(
+      //   screen.getByTestId("icon-window").classList.contains("active-icon")
+      // ).toBe(true);
+    });
     test("Then bills should be ordered from earliest to latest", () => {
-      const html = BillsUI({ data: bills })
-      document.body.innerHTML = html
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
-})
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+  });
+});
+
+describe("Given I am connected as Employee and I am on Bills page", () => {
+  describe("When I click on the icon eye", () => {
+    test("A modal should open", () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const firestore = null;
+      const allBills = new Bills({
+        document,
+        onNavigate,
+        firestore,
+        localStorage: window.localStorage,
+      });
+
+      $.fn.modal = jest.fn();
+      const eye = screen.getAllByTestId("icon-eye")[0];
+      const handleClickIconEye = jest.fn(() =>
+        allBills.handleClickIconEye(eye)
+      );
+      eye.addEventListener("click", handleClickIconEye);
+      fireEvent.click(eye);
+      expect(handleClickIconEye).toHaveBeenCalled();
+      const modale = document.getElementById("modaleFile");
+      expect(modale).toBeTruthy();
+    });
+  });
+});
